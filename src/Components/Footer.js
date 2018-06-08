bd656138a1a2bf91cc9542462c214c2bdc23640b
Alex <doll3098@ou.edu>
2018-06-08 17:54:08 +0000

    add social media links to footer and style footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,17 +1,26 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import '../Stylesheets/Footer.css';
 
 class Footer extends Component {
   render() {
     let {onViewLandingClick} = this.props;
     
     return (
-      <footer>
+      <footer className='navbar nav footer-custom'>
         <Link to='/'>
-          <button onClick={onViewLandingClick}>
+          <button className='btn btn-link' onClick={onViewLandingClick}>
             View Landing
           </button>
         </Link>
+        <div className='social-media'>
+          <a className='btn btn-link' href='https://github.com/Rosco1010' target='_blank' rel="noopener noreferrer">
+            <i className="fab fa-github fa-2x"></i>
+          </a>
+          <a className='btn btn-link' href='https://www.linkedin.com/in/alexander-doll/' target='_blank' rel="noopener noreferrer">
+            <i className="fab fa-linkedin fa-2x"></i>
+          </a>
+        </div>
       </footer>
     );
   }
