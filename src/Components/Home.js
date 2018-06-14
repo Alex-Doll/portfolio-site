59f3a4cfb2482afc92f0f8363339d8b2cae17562
Alex <doll3098@ou.edu>
2018-06-14 22:21:54 +0000

    add more bootstrap layout styles to the Home component

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,33 +4,16 @@ import '../Stylesheets/Home.css';
 class Home extends Component {
   render() {
     return (
-      <section className='home container'>
+      <section className='jumbotron jumbotron-fluid home-custom'>
         <div className='container'>
-          <div>
-            <h3>Greetings Adventurer</h3>
-            <h1>My name is Alex Doll</h1>
-            <h1>I'm a web developer based in Austin, TX.</h1>
-            <p>This portfolio is yours to explore. 
-            Whether you want to learn more about me or simply check out some of my work, 
-            there is plenty for you to enjoy!</p>
-          </div>
-          <p className='joke-line'>The real treasure all along was my portfolio...or was it friendship?</p>
-          <ul>
-            <li>list item!</li>
-            <li>list item!</li>
-            <li>list item!</li>
-            <li>list item!</li>
-            <li>list item!</li>
-            <li>list item!</li>
-            <li>list item!</li>
-            <li>list item!</li>
-            <li>list item!</li><li>list item!</li>
-            
-            <li>list item!</li>
-            <li>list item!</li>
-            <li>list item!</li>
-            <li>list item!</li>
-          </ul>
+          <h3>Greetings Adventurer</h3>
+          <hr />
+          <h1 className='display-2'>My name is Alex Doll</h1>
+          <h1 className='display-2'>I'm a web developer based in Austin, TX.</h1>
+          <p className='lead'>This portfolio is yours to explore. 
+          Whether you want to learn more about me or simply check out some of my work, 
+          there is plenty for you to enjoy!</p>
+          <p>The real treasure all along was my portfolio...or was it friendship?</p>
         </div>
       </section>
     );
