d7600befaee1ab675771b18835f48b93804bbfaf
Alex <doll3098@ou.edu>
2018-06-15 17:45:58 +0000

    Add more font and whitespace styling to all components

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,16 +4,17 @@ import '../Stylesheets/About.css';
 class About extends Component {
   render() {
     return (
-      <div className='about'>
+      <div className='about-custom'>
         <div className='container text-left'>
-          <p>Hello there! If you're reading this then you probably are wanting to get to know a little bit more about me. If that's not why you're on this page
+          <p className='about-important'>Hello there!</p>
+          <p>If you're reading this then you probably are wanting to get to know a little bit more about me. If that's not why you're on this page
           then hopefully you'll be delighted nonetheless!</p>
           <p>Once again, my name is Alex. I'm a full-stack software developer with a passion for creating things with code. I'm currently looking for
           employment as a developer so that I can apply and strengthen my skills all day everyday. From focusing in on a solo project to collaborating with
           larger teams, my biggest indicators for success are honesty and open communication. These important skills, along with a passion to always 
           continue learning are my core beliefs in regards to work ethic and software development in general!</p>
           <p>Thanks again for stopping by my portfolio and have a great rest of your day!!!</p>
-          <p>- Alex</p>
+          <p className='about-important'>- Alex</p>
         </div>
       </div>
     );
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -8,12 +8,11 @@ class Home extends Component {
         <div className='container text-left'>
           <h3>Greetings Adventurer</h3>
           <hr />
-          <h1 className='display-2'>My name is Alex Doll</h1>
-          <h1 className='display-2'>I'm a web developer based in Austin, TX.</h1>
+          <h1 className='display-2'>My name is Alex Doll<br />I'm a web developer based in Austin, TX.</h1>
           <p className='lead'>This portfolio is yours to explore. 
           Whether you want to learn more about me or simply check out some of my work, 
           there is plenty for you to enjoy!</p>
-          <p>The real treasure all along was my portfolio...or was it friendship?</p>
+          <p><em>The real treasure all along was my portfolio...or was it friendship?</em></p>
         </div>
       </section>
     );
