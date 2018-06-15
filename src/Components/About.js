3d3f9d6b58c159124ff8006070ea81b483266310
Alex <doll3098@ou.edu>
2018-06-15 04:14:19 +0000

    Update text on the About component

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,8 +6,14 @@ class About extends Component {
     return (
       <div className='about'>
         <div className='container'>
-          <p>This Is The About Page! While the page is still currently under construction, please feel free to view my work by clicking on the GitHub icon
-          in the footer below!</p>
+          <p>Hello there! If you're reading this then you probably are wanting to get to know a little bit more about me. If that's not why you're on this page
+          then hopefully you'll be delighted nonetheless!</p>
+          <p>Once again, my name is Alex. I'm a full-stack software developer with a passion for creating things with code. I'm currently looking for
+          employment as a developer so that I can apply and strengthen my skills all day everyday. From focusing in on a solo project to collaborating with
+          larger teams, my biggest indicators for success are honesty and open communication. These important skills, along with a passion to always 
+          continue learning are my core beliefs in regards to work ethic and software development in general!</p>
+          <p>Thanks again for stopping by my portfolio and have a great rest of your day!!!</p>
+          <p>- Alex</p>
         </div>
       </div>
     );
