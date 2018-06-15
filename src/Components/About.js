09604f68f662fde3f59c06418653e911d094d846
Alex <doll3098@ou.edu>
2018-06-15 18:12:30 +0000

    Finish styling the Project component and About component

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,15 +6,31 @@ class About extends Component {
     return (
       <div className='about-custom'>
         <div className='container text-left'>
-          <p className='about-important'>Hello there!</p>
-          <p>If you're reading this then you probably are wanting to get to know a little bit more about me. If that's not why you're on this page
-          then hopefully you'll be delighted nonetheless!</p>
-          <p>Once again, my name is Alex. I'm a full-stack software developer with a passion for creating things with code. I'm currently looking for
-          employment as a developer so that I can apply and strengthen my skills all day everyday. From focusing in on a solo project to collaborating with
-          larger teams, my biggest indicators for success are honesty and open communication. These important skills, along with a passion to always 
-          continue learning are my core beliefs in regards to work ethic and software development in general!</p>
-          <p>Thanks again for stopping by my portfolio and have a great rest of your day!!!</p>
-          <p className='about-important'>- Alex</p>
+          <p className='about-important'><strong>Hello there!</strong></p>
+          <p>If you're reading this then you probably are wanting to <strong>get to know a little bit more about me</strong>. If that's not why you're
+          on this page then hopefully you'll be delighted nonetheless!</p>
+          
+          <hr />
+          
+          <ul>
+            <li>
+              <p>Once again, <strong>my name is Alex</strong>.</p>
+            </li>
+            <li>
+              <p>I'm a <strong>full-stack software developer</strong> with a passion for creating things with code.</p>
+            </li>
+            <li>
+              <p>I'm currently <strong>looking for employment</strong> as a developer so that I can apply and strengthen my skills all day everyday.</p>
+            </li>
+            <li>
+              <p>From focusing in on a solo project to collaborating with larger teams, <strong>my biggest indicators for success are honesty and open 
+              communication.</strong> These important skills, along with <strong>a passion to always continue learning</strong> are my core beliefs
+              in regards to work ethic and software development in general!</p>
+            </li>
+          </ul>
+             
+          <p><strong>Thanks again</strong> for stopping by my portfolio and have a great rest of your day!!!</p>
+          <p className='about-important'><strong>- Alex</strong></p>
         </div>
       </div>
     );
--- src/Components/Project.js
+++ src/Components/Project.js
@@ -10,7 +10,7 @@ class Project extends Component {
           <div className='card'>
             <img className="card-img-top" src={imgSrc} alt={title} />
             <div className="card-body">
-              <h5 className="card-title">{title}</h5>
+              <h5 className="card-title"><strong>{title}</strong></h5>
               <p className="card-text">{description}</p>
               <a href={linkTo} className="btn btn-primary" target='_blank' rel="noopener noreferrer"><strong>View Project!</strong></a>
             </div>
